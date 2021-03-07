7b73b76a98251d30dab4f77af1e575f770b9a6de
dnerini <daniele.nerini@gmail.com>
2021-03-07 17:35:00 +0100

    Some refactoring

--- startleiter/xcontest.py
+++ startleiter/xcontest.py
@@ -1,25 +1,21 @@
-import getpass
 import logging
 import time
-from pprint import pprint
 
-import pandas as pd
-import psutil
 import requests
 from bs4 import BeautifulSoup
-from selenium import webdriver
+
 from selenium.common.exceptions import TimeoutException, StaleElementReferenceException
 from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
 from startleiter.database import Database
+from startleiter.scraping import launch_browser, pacing, wait_till_loaded
 
 logger = logging.getLogger(__name__)
 
 COUNTER = 0
-
+STOP_AFTER = 1e6
 BASE_URL = "https://www.xcontest.org"
 SEARCH_URL = BASE_URL + "/world/en/flights-search"
 DEFAULT_QUERY = {
@@ -40,20 +36,18 @@ DEFAULT_QUERY = {
 }
 
 
-def launch_browser():
-    options = Options()
-    options.headless = True
-    # options.log.level = "trace"  # log output is stored in geckodriver.log (current dir)
-    browser = webdriver.Firefox(
-        options=options,
-        executable_path="/home/ned/.local/bin/geckodriver"
-    )
-    browser.set_page_load_timeout(5)
-    browser.set_script_timeout(5)
-    return browser
+def build_query(parameters=None):
+    """Build search url for xcontest.org.
+    
+    Parameters
+    ----------
+    parameters: dict
+        Optional parameters to change wrt the default query.
 
-
-def query_flights(parameters=None):
+    Returns
+    -------
+    query_url: str
+    """
     if parameters is None:
         parameters = {}
     query = DEFAULT_QUERY.copy()
@@ -62,7 +56,37 @@ def query_flights(parameters=None):
     return SEARCH_URL + "/?" + query_str
 
 
-def parse_table(soup, class_name):
+def flight_summary(table_row):
+    """Parse flight summary data from a given query.
+
+    Parameters
+    ----------
+    table_row
+
+    Returns
+    -------
+    summary: list
+        flid, no., start time, pilot, launch, route, length, points, glider_class, glider, route
+    """
+    cols = table_row.find_all("td")  # 10 columns
+    text = [ele.text.strip() for ele in cols][:-2]
+    flid = cols[0].get("title").split(":")[1]
+    glider = cols[7].find_all("div")[0].get("title")
+    route = cols[4].find_all("div")[0].get("title")
+    summary = [flid, *text, glider, route]
+    logger.info(summary)
+    return summary
+
+
+def _flight_details_href(row):
+    for div in row.find_all("div"):
+        details = div.find_all("a", {"class": "detail"})
+        if details:
+            return details[0].get("href")
+    return None
+
+
+def _parse_table(soup, class_name):
     table = soup.find("table", attrs={"class": class_name})
     table_body = table.find("tbody")
     rows = table_body.find_all("tr")
@@ -74,37 +98,27 @@ def parse_table(soup, class_name):
     return content
 
 
-def flight_details_href(row):
-    for div in row.find_all("div"):
-        details = div.find_all("a", {"class": "detail"})
-        if details:
-            return details[0].get("href")
-    return None
+def flight_details(flight, browser):
+    """Parse flight detail data from a given query.
 
+    Parameters
+    ----------
+    flight
+    browser
 
-def wait_till_loaded(browser, url, maxattempts=3):
-    try:
-        pageid = url.split("/detail:")[1]
-    except IndexError:
-        return 0
-    attempt = 0
-    while pageid not in browser.current_url:
-        time.sleep(2 ** attempt)
-        attempt += 1
-        if attempt > maxattempts:
-            return 0
-    return 1
-
-
-def flight_details(flight):
-    href = flight_details_href(flight)
+    Returns
+    -------
+    details: list
+        airtime, max. altitude, max. alt. gain, max. climb, max. sink, tracklog length, free distance
+    """
+    href = _flight_details_href(flight)
     if not href:
         return None
     url = BASE_URL + href
     logger.info(url)
 
+    browser.delete_all_cookies()
     try:
-        browser.delete_all_cookies()
         browser.get(url + "#fd=flight")
     except Exception as e:
         logger.error(e)
@@ -124,7 +138,7 @@ def flight_details(flight):
     soup = BeautifulSoup(browser.page_source, "html.parser")
     subsoup = soup.find("div", attrs={"class": "XCmoreInfo"})
     try:
-        details = parse_table(subsoup, "XCinfo")
+        details = _parse_table(subsoup, "XCinfo")
     except Exception as ex:
         logger.error(ex)
         return None
@@ -133,86 +147,109 @@ def flight_details(flight):
     return details
 
 
-def as_dataframe(data):
-    column_names = ["flid", "no.", "start time", "pilot", "launch", "route", "length", "points", "glider_class", "glider"]
-    column_names += ["airtime", "max. altitude", "max. alt. gain", "max. climb"]
-    column_names += ["max. sink", "tracklog length", "free distance"]
-    df = pd.DataFrame(data, columns=column_names)
-    df.set_index("No.", inplace=True)
-    return df
+def parse_flights(query_url, browser, pace=120):
+    """Loop all flights for a given query on xcontest.org, extract
+    flight summary and details.
 
-def parse_flight(row):
-    cols = row.find_all("td")  # 10 columns
-    text = [ele.text.strip() for ele in cols][:-2]
-    flid = cols[0].get("title").split(":")[1]
-    glider = cols[7].find_all("div")[0].get("title")
-    route = cols[4].find_all("div")[0].get("title")
-    return [flid, *text, glider, route]
+    Parameters
+    ----------
+    query_url: str
+        A search query url for xcontest.org.
+    browser: selenium.WebDriver
+        A selenium-webdriver client.
+    pace: int
+        The desired pacing in number of requests per hour.
 
+    Returns
+    -------
 
-def parse_flights(query_url, pace):
+    """
     global COUNTER
+
     page = requests.get(query_url)
     soup = BeautifulSoup(page.content, "html.parser")
     table = soup.find("table", attrs={"class": "flights"})
     table_body = table.find("tbody")
-    rows = table_body.find_all("tr")
-    flights = []
+    flights = table_body.find_all("tr")
+
     t0 = time.monotonic()
     total_sleep = 0
     consecutive_timeouts = 0
-    for n, row in enumerate(rows):
+    flights_data = []
+    for n, flight in enumerate(flights):
+
+        flight_data = []
+
+        # Parse flight summary
         try:
-            flight = parse_flight(row)
+            summary = flight_summary(flight)
         except IndexError:
             logger.error(f"Failed to parse flight {n}")
             continue
-        logger.info(flight)
+        flight_data += summary
+
+        # Parse flight details
         try:
-            details = flight_details(row)
+            details = flight_details(flight, browser)
         except TimeoutException:
             consecutive_timeouts += 1
-            details = None
+            details = [None, ] * 7
         else:
             consecutive_timeouts = 0
+        flight_data += details
+
         if consecutive_timeouts > 3:
             raise TimeoutException
-        if details:
-            flight += details
-        else:
-            flight += [None, ] * 7
-        flights.append(flight)
-        # pacing and spacing
-        time_lapsed = time.monotonic() - t0
-        requests_done = n + 1
-        requests_togo = len(rows) - requests_done
-        logger.info(f"Averaging {requests_done / time_lapsed * 3600:.0f} requests per hour")
-        if requests_togo > 0:
-            time_togo = (requests_done + requests_togo) / pace * 3600 - time_lapsed
-            spacing = max(0, time_togo / requests_togo - (time_lapsed - total_sleep) / requests_done)
-            if spacing > 0:
-                logger.info(f"Slowing down... wait {spacing:.1f} seconds...")
-                time.sleep(spacing)
-                total_sleep += spacing
+
+        # TODO: Parse flight track
+
+        flights_data.append(flight_data)
+
+        total_sleep = pacing(n, len(flights), t0, pace, total_sleep)
 
         COUNTER += 1
-        if COUNTER == stop_after:
+        if COUNTER == STOP_AFTER:
             break
-    return flights
+
+    return flights_data
 
 
-def cleanup():
-    for proc in psutil.process_iter():
-        if (proc.name() in ["geckodriver", "Web Content", "firefox-bin"]
-                and proc.username() == getpass.getuser()):
-            try:
-                proc.kill()
-            except psutil.NoSuchProcess:
-                pass
+def scrape(source, site, pace):
+    """Main scraping routine for xcontest.org
+    """
+    # Connect to database
+    db = Database(source, site)
+
+    browser = launch_browser()
+    time_start = time.monotonic()
+
+    try:
+        id_start = db.query_last_flight()
+        while id_start < STOP_AFTER:
+            logger.info(f"XContest Flight Chunk {id_start} to {id_start + min(STOP_AFTER - 1, 50)}")
+            query_url = build_query({"list[start]": id_start})
+            flight_chunk = parse_flights(query_url, browser, pace)
+            if flight_chunk:
+                db.insert_flights(flight_chunk)
+                id_start += 50
+            else:
+                break
+
+    except TimeoutException:
+        time_lapsed = time.monotonic() - time_start
+        logger.error(f"Timeout after {COUNTER} queries and {int(time_lapsed / 60)} minutes.")
+
+    finally:
+        browser.quit()
+        # cleanup()
+        df = db.to_pandas()
+        print(df)
+        print(df.describe())
 
 
 if __name__ == "__main__":
 
+    # Setup logging
     logging.basicConfig(
         # format="%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s",
         format="%(levelname)-4s [%(filename)s:%(lineno)d] %(message)s",
@@ -235,33 +272,6 @@ if __name__ == "__main__":
         "radius": 1000,
     }
 
-    # Connect to database
-    db = Database(source, site)
-
-    # set pacing in number of requests per hour
-    pace = 120
-    stop_after = 1e5
-
-    browser = launch_browser()
-    try:
-        time_start = time.monotonic()
-        id_start = db.query_last_flight()
-        while id_start < stop_after:
-            logger.info(f"XContest Flight Chunk {id_start} to {id_start + min(stop_after - 1, 50)}")
-            flight_chunk = parse_flights(query_flights({"list[start]": id_start}), pace)
-            if flight_chunk:
-                db.insert_flights(flight_chunk)
-                id_start += 50
-            else:
-                break
-    except TimeoutException:
-        time_lapsed = time.monotonic() - time_start
-        logger.error(f"Timeout after {COUNTER} queries and {int(time_lapsed / 60)} minutes.")
-
-    finally:
-        browser.quit()
-        # cleanup()
-
-        df = db.to_pandas()
-        print(df)
-        print(df.describe())
+    # Set pace and start main routine
+    pace = 120  # requests / hour
+    scrape(source, site, pace)
