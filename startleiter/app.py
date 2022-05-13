0bf388f8bf547718acc9d0ac2354a54b820a2b7f
Daniele Nerini <daniele.nerini@gmail.com>
2022-05-13 21:10:35 +0200

    Add forecast mode

--- startleiter/app.py
+++ startleiter/app.py
@@ -1,5 +1,7 @@
-from datetime import datetime
+from datetime import datetime, timedelta
+from functools import lru_cache
 from io import BytesIO
+from typing import Optional
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -8,24 +10,33 @@ import xarray as xr
 from fastapi import FastAPI
 from starlette.responses import StreamingResponse, RedirectResponse
 
+from startleiter import config as CFG
+from startleiter import uwyo, rucsoundings
 from startleiter.decorators import try_wait
 from startleiter.explainer import compute_shap, explainable_plot
 from startleiter.utils import to_wind_components
-from startleiter.uwyo import scrape
 
 app = FastAPI()
 
 
 # TODO: do not hardcode
+STATIONS = CFG["stations"]
 CIMETTA_ELEVATION = 1600
 ALT_BIN = 300
 DIST_BIN = 15
 
 
+@lru_cache
 @try_wait()
-def get_last_sounding(station):
-    time = datetime.utcnow().replace(hour=0, minute=0, second=0, microsecond=0)
-    data = list(scrape(station, time).items())[0]
+def get_last_sounding(station, time):
+    data = list(uwyo.scrape(station, time).items())[0]
+    return data[0], data[1]["data"]
+
+
+@lru_cache
+def get_last_sounding_forecast(station, time, leadtime_hrs):
+    leadtime = timedelta(hours=leadtime_hrs)
+    data = list(rucsoundings.scrape(station, time, leadtime).items())[0]
     return data[0], data[1]["data"]
 
 
@@ -33,12 +44,14 @@ def preprocess(ds):
     ds = to_wind_components(ds)
     # dew point temperature depression
     ds["DWPD"] = ds["TEMP"] - ds["DWPT"]
-    return (ds[["TEMP", "DWPD", "U", "V"]]
-            .rename({"PRES": "level"})
-            .bfill(dim="level", limit=3)
-            .to_array().transpose("level", "variable")
-            .astype("float32")
-            )
+    return (
+        ds[["TEMP", "DWPD", "U", "V"]]
+        .rename({"PRES": "level"})
+        .bfill(dim="level", limit=3)
+        .to_array()
+        .transpose("level", "variable")
+        .astype("float32")
+    )
 
 
 def standardize(da, moments, inverse=False):
@@ -49,24 +62,33 @@ def standardize(da, moments, inverse=False):
         return da * moments.sigma + moments.mu
 
 
-def pipeline(station):
+def pipeline(station, leadtime_days):
     """Clean and preprocess the input data"""
-    validtime, sounding = get_last_sounding(station)
+    time = datetime.utcnow().replace(hour=0, minute=0, second=0, microsecond=0)
+    station = STATIONS[station]
+    if leadtime_days is not None:
+        validtime, sounding = get_last_sounding_forecast(
+            station["name"], time, leadtime_hrs=int(leadtime_days) * 24
+        )
+        sounding.attrs["source"] = f"GFS sounding +{leadtime_days * 24:.0f} h"
+    else:
+        validtime, sounding = get_last_sounding(station["stid"], time)
+        sounding.attrs["source"] = f"Radiosounding 00Z {station['long_name']}"
     sounding = preprocess(sounding)
     sounding.attrs["validtime"] = validtime
     return sounding
 
 
-@app.get('/', include_in_schema=False)
+@app.get("/", include_in_schema=False)
 async def basic_view():
-    return RedirectResponse('/docs')
+    return RedirectResponse("/docs")
 
 
-@app.get('/cimetta')
-async def predict():
+@app.get("/cimetta")
+async def predict_cimetta(leadtime_days: Optional[int] = None):
 
     # get inputs
-    sounding = pipeline(16064)
+    sounding = pipeline("Cameri", leadtime_days)
     validtime = sounding.attrs["validtime"]
 
     # fly prob
@@ -81,7 +103,9 @@ async def predict():
     inputs = standardize(sounding, moments).values[None, ...]
     # max_alt = int(model.predict(inputs)[0].argmax() * ALT_BIN + CIMETTA_ELEVATION)
     pred = np.array(model.predict(inputs)[0])
-    max_alt = np.sum(pred * (np.arange(pred.size) * ALT_BIN)) // 100 * 100 + CIMETTA_ELEVATION
+    max_alt = (
+        np.sum(pred * (np.arange(pred.size) * ALT_BIN)) // 100 * 100 + CIMETTA_ELEVATION
+    )
 
     # max distance
     model = tf.keras.models.load_model("models/fly_max_dist_1.h5")
@@ -98,11 +122,11 @@ async def predict():
     }
 
 
-@app.get('/cimetta_plot')
-async def explain():
+@app.get("/cimetta_plot")
+async def explain_cimetta(leadtime_days: Optional[int] = None):
 
     # get inputs
-    sounding = pipeline(16064)
+    sounding = pipeline("Cameri", leadtime_days)
 
     # fly prob
     model = tf.keras.models.load_model("models/fly_prob_1.h5")
@@ -114,7 +138,6 @@ async def explain():
 
     # plot  shap
     fig = explainable_plot(sounding, shap_values, fly_prob)
-    fig.tight_layout()
     image_file = BytesIO()
     plt.savefig(image_file)
     plt.close(fig)
--- startleiter/explainer.py
+++ startleiter/explainer.py
@@ -2,16 +2,21 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 import shap
+import tensorflow as tf
 from metpy.plots import SkewT
 from metpy.units import units
 
+# https://github.com/slundberg/shap/issues/2189#issuecomment-1048384801
+tf.compat.v1.disable_v2_behavior()
+
 
 def compute_shap(background, model, inputs):
-    e = shap.DeepExplainer(model, background)
+    # e = shap.DeepExplainer(model, background)
+    e = shap.DeepExplainer((model.layers[0].input, model.layers[-1].output), background)
     return e.shap_values(inputs)
 
 
-def explainable_plot(sounding, shap_values, prediction):
+def explainable_plot(sounding, shap_values, prediction, min_pressure_hPa=400):
     # Assign units
     p = sounding.level.values * units.hPa
     T = sounding.sel(variable="TEMP").values * units.degC
@@ -19,8 +24,8 @@ def explainable_plot(sounding, shap_values, prediction):
     U = sounding.sel(variable="U").values * units.knots
     V = sounding.sel(variable="V").values * units.knots
 
-    fig = plt.figure(figsize=(6, 5), dpi=300)
-    skew = SkewT(fig, rotation=45)
+    fig = plt.figure(figsize=(4.6, 5), dpi=300)
+    skew = SkewT(fig, rotation=45, aspect=100)
 
     # SHAP values
     shval = shap_values[0, :, :]
@@ -31,67 +36,135 @@ def explainable_plot(sounding, shap_values, prediction):
     shval /= shmax
     shval = np.clip(shval, -1, 1)
     for i in range(len(p) - 1):
-        skew.plot(p[i:i + 2], T[i:i + 2], lw=5, color='r', alpha=np.clip(shval[i, 0], 0, 1))
-        skew.plot(p[i:i + 2], T[i:i + 2], lw=5, color='b', alpha=np.abs(np.clip(shval[i, 0], -1, 0)))
-        skew.plot(p[i:i + 2], Td[i:i + 2], lw=5, color='r', alpha=np.clip(shval[i, 1], 0, 1))
-        skew.plot(p[i:i + 2], Td[i:i + 2], lw=5, color='b', alpha=np.abs(np.clip(shval[i, 1], -1, 0)))
-        skew.plot_barbs(p[i:i + 2], U[i:i + 2], V[i:i + 2], color='r', alpha=np.clip(shval[i, 2:].mean(), 0, 1))
-        skew.plot_barbs(p[i:i + 2], U[i:i + 2], V[i:i + 2], color='b', alpha=np.abs(np.clip(shval[i, 2:].mean(), -1, 0)))
+        if p[i + 1] < min_pressure_hPa * units.hPa:
+            continue
+        skew.plot(
+            p[i : i + 2],
+            T[i : i + 2],
+            lw=5,
+            color="r",
+            alpha=np.clip(shval[i, 0], 0, 1),
+        )
+        skew.plot(
+            p[i : i + 2],
+            T[i : i + 2],
+            lw=5,
+            color="b",
+            alpha=np.abs(np.clip(shval[i, 0], -1, 0)),
+        )
+        skew.plot(
+            p[i : i + 2],
+            Td[i : i + 2],
+            lw=5,
+            color="r",
+            alpha=np.clip(shval[i, 1], 0, 1),
+        )
+        skew.plot(
+            p[i : i + 2],
+            Td[i : i + 2],
+            lw=5,
+            color="b",
+            alpha=np.abs(np.clip(shval[i, 1], -1, 0)),
+        )
+        skew.plot_barbs(
+            p[i : i + 2],
+            U[i : i + 2],
+            V[i : i + 2],
+            color="r",
+            alpha=np.clip(shval[i, 2:].mean(), 0, 1),
+        )
+        skew.plot_barbs(
+            p[i : i + 2],
+            U[i : i + 2],
+            V[i : i + 2],
+            color="b",
+            alpha=np.abs(np.clip(shval[i, 2:].mean(), -1, 0)),
+        )
 
-    skew.plot(p, T, 'k')
-    skew.plot(p, Td, '--k')
+    skew.plot(p, T, "k")
+    skew.plot(p, Td, "--k")
+    skew.ax.set_xlabel("Temperature (\N{DEGREE CELSIUS})", fontdict=dict(size="small"))
+    skew.ax.set_ylabel("Pressure (hPa)", fontdict=dict(size="small"))
 
-    skew.ax.set_ylim(1000, 400)
-    skew.ax.set_xlim(Td.magnitude[0] - 20, Td.magnitude[0] + 20)
+    skew.ax.set_ylim(1000, min_pressure_hPa)
+    skew.ax.set_xlim(Td.magnitude[0] - 18, Td.magnitude[0] + 17)
 
-    cmap = plt.get_cmap('bwr')
+    cmap = plt.get_cmap("bwr")
     norm = mpl.colors.Normalize(vmin=-1, vmax=1)
     sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
     sm.set_array([])
-    cbaxes = fig.add_axes([0.945, 0.55, 0.02, 0.25])
+    cbaxes = fig.add_axes([0.93, 0.57, 0.015, 0.25])
     cbar = plt.colorbar(sm, cax=cbaxes)
-    cbar.ax.set_title('Favourable', fontsize="x-small")
-    cbar.ax.set_xlabel('Adverse', fontsize="x-small")
     cbar.set_ticks([])
+    cbar.ax.text(
+        0.5,
+        1.0,
+        "Favourable",
+        transform=cbar.ax.transAxes,
+        va="bottom",
+        ha="center",
+        fontsize="xx-small",
+    )
+    cbar.ax.text(
+        0.5,
+        -0.01,
+        "Adverse",
+        transform=cbar.ax.transAxes,
+        va="top",
+        ha="center",
+        fontsize="xx-small",
+    )
 
     validtime = f"{sounding.attrs['validtime']:%Y-%m-%d}"
+    source = f"{sounding.attrs['source']}"
 
     text_sx_top = f"""
-Cimetta, Switzerland (1600 masl)
-{validtime}
-Radiosounding 00Z 16064 Cameri"""
-    skew.ax.text(0, 1.01, text_sx_top,
-             fontsize="small",
-             stretch="condensed",
-             linespacing=1.1,
-             va="bottom",
-             transform=skew.ax.transAxes
-             )
+Target: Cimetta, Switzerland (1600 masl)
+Valid: {validtime}
+Input: {source}"""
+    skew.ax.text(
+        0,
+        1.01,
+        text_sx_top,
+        fontsize="small",
+        stretch="condensed",
+        linespacing=1.1,
+        va="bottom",
+        transform=skew.ax.transAxes,
+    )
 
     text_dx_top = f"""
-Flying probability: {prediction * 100:.0f}%"""
-    skew.ax.text(1.0, 1.01, text_dx_top,
-             fontsize="small",
-             stretch="condensed",
-             linespacing=1.1,
-             ha="right",
-             va="bottom",
-             transform=skew.ax.transAxes
-             )
+flyability {prediction * 100:.0f}%"""
+    skew.ax.text(
+        1.0,
+        1.01,
+        text_dx_top,
+        fontsize="small",
+        fontweight="bold",
+        stretch="condensed",
+        linespacing=1.1,
+        ha="right",
+        va="bottom",
+        transform=skew.ax.transAxes,
+    )
 
     text_sx_bottom = f"""
 Credits:
+    - Radiosoundings: weather.uwyo.edu
+    - GFS soundings: rucsoundings.noaa.gov
     - Flight data: xcontest.org
-    - Radiosounding data: weather.uwyo.edu
     - Impact score: github.com/slundberg/shap
     - SkewT plot: unidata.github.io/MetPy"""
-    skew.ax.text(0.01, 0.01, text_sx_bottom,
-             fontsize="x-small",
-             stretch="condensed",
-             linespacing=1.1,
-             ha="left",
-             va="bottom",
-             transform=skew.ax.transAxes
-             )
+    skew.ax.text(
+        0.01,
+        0.01,
+        text_sx_bottom,
+        fontsize="xx-small",
+        stretch="condensed",
+        linespacing=1.1,
+        ha="left",
+        va="bottom",
+        transform=skew.ax.transAxes,
+    )
 
     return fig
