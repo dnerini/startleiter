c18554fdefeb5dc6d5c2da0ab2fc961b6cd4cef5
Daniele Nerini <daniele.nerini@gmail.com>
2022-12-09 21:47:58 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="startleiter",
-    version="0.0.1",
+    version="0.0.2",
     url="https://github.com/dnerini/startleiter.git",
     author="Daniele Nerini",
     author_email="daniele.nerini@gmail.com",
