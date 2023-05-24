c5da585953da7542fbd0a8c68c0ff41830911ca8
Daniele Nerini <daniele.nerini@gmail.com>
2023-05-24 23:09:38 +0200

    Update plot

--- startleiter/plots.py
+++ startleiter/plots.py
@@ -2,6 +2,8 @@ import matplotlib as mpl
 import matplotlib.dates as mdates
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+from matplotlib.dates import DateFormatter
 from metpy.plots import SkewT
 from metpy.units import units
 
@@ -25,12 +27,21 @@ def text_validtime(validtime):
 {validtime}"""
 
 
-def text_results(flyability, max_alt_m, max_dist_km, source):
+def text_plot(site, validtime, flyability, max_alt_m, max_dist_km, source):
     return f"""
+{site['name']}, Switzerland ({site['elevation']} masl)
+{validtime}
+
 Flyability: {flyability * 100:.0f}%
 Max flying height: {max_alt_m} m
 Max flying distance: {max_dist_km} km
-Input: {source}"""
+
+Profile:
+{source}
+
+QFF gradients:
+DWD-ICON
+"""
 
 
 def explainable_plot(
@@ -43,15 +54,18 @@ def explainable_plot(
     Td = (T.magnitude - features.sel(variable="DWPD").values) * units.degC
     U = features.sel(variable="U").values * units.knots
     V = features.sel(variable="V").values * units.knots
+    QFF_KL = features.sel(variable="KLO-LUG").values * units.hPa
+    QFF_KG = features.sel(variable="KLO-GVE").values * units.hPa
 
-    fig = plt.figure(figsize=(4.5, 5.5), dpi=300)
-    skew = SkewT(fig, rotation=45, aspect=100)
+    fig = plt.figure(layout=None, figsize=(6, 6.5))
+    gs = fig.add_gridspec(nrows=5, ncols=3)
+    ax1 = fig.add_subplot(gs[:3, 2])
+    ax2 = fig.add_subplot(gs[3, :])
+    ax3 = fig.add_subplot(gs[4, :])
+    skew = SkewT(fig, aspect=100, rotation=45, subplot=gs[:3, :2])
 
     # SHAP values
     shval = shap_values[0, :, :]
-    # split DWPD between TEMP and DWPT
-    # shval[:, 0] += shval[:, 1] / 2
-    # shval[:, 1] /= 2
     shmax = np.quantile(np.abs(shval), 0.98)
     shval /= shmax
     shval = np.clip(shval, -1, 1)
@@ -115,7 +129,7 @@ def explainable_plot(
     norm = mpl.colors.Normalize(vmin=-1, vmax=1)
     sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
     sm.set_array([])
-    cbaxes = fig.add_axes([0.19, 0.22, 0.015, 0.1])
+    cbaxes = fig.add_axes([0.65, 0.45, 0.015, 0.1])
     cbar = plt.colorbar(sm, cax=cbaxes)
     cbar.set_ticks([])
     cbar.ax.text(
@@ -125,7 +139,7 @@ def explainable_plot(
         transform=cbar.ax.transAxes,
         va="bottom",
         ha="center",
-        fontsize="xx-small",
+        fontsize="x-small",
     )
     cbar.ax.text(
         0.5,
@@ -134,55 +148,88 @@ def explainable_plot(
         transform=cbar.ax.transAxes,
         va="top",
         ha="center",
-        fontsize="xx-small",
+        fontsize="x-small",
     )
     validtime = f"{features.attrs['validtime']:%Y-%m-%d}"
     source = f"{features.attrs['source']}"
-    skew.ax.text(
+    ax1.text(
         0,
-        1.05,
-        text_title(),
-        fontsize="xx-large",
-        stretch="condensed",
-        linespacing=1.1,
-        va="bottom",
-        ha="left",
-        transform=skew.ax.transAxes,
-    )
-    skew.ax.text(
-        0,
-        1.01,
-        text_site(site),
-        fontsize="small",
-        stretch="condensed",
-        linespacing=1.1,
-        ha="left",
-        va="bottom",
-        transform=skew.ax.transAxes,
-    )
-    skew.ax.text(
         1.0,
-        1.01,
-        text_validtime(validtime),
-        fontsize="small",
+        text_title(),
+        fontsize="x-large",
         stretch="condensed",
         linespacing=1.1,
-        ha="right",
         va="bottom",
-        transform=skew.ax.transAxes,
+        ha="left",
+        transform=ax1.transAxes,
     )
-    plt.subplots_adjust(bottom=0.2)
-    skew.ax.text(
-        0.0,
-        -0.1,
-        text_results(flyability, max_alt_m, max_dist_km, source),
+    ax1.text(
+        0,
+        1,
+        text_plot(site, validtime, flyability, max_alt_m, max_dist_km, source),
         fontsize="small",
         stretch="condensed",
         linespacing=1.1,
         ha="left",
         va="top",
-        transform=skew.ax.transAxes,
+        transform=ax1.transAxes,
     )
+
+    ax1.axis("off")
+
+    tt = pd.date_range(validtime, periods=24, freq="1H")
+    ax2.plot(tt, QFF_KG[:24], "k-")
+    ax3.plot(tt, QFF_KL[:24], "k-")
+    for i in range(len(tt) - 1):
+        ax2.plot(
+            tt[i : i + 2],
+            QFF_KG[i : i + 2],
+            lw=5,
+            color="r",
+            alpha=np.clip(shval[i, 4], 0, 1),
+        )
+        ax2.plot(
+            tt[i : i + 2],
+            QFF_KG[i : i + 2],
+            lw=5,
+            color="b",
+            alpha=np.abs(np.clip(shval[i, 4], -1, 0)),
+        )
+        ax3.plot(
+            tt[i : i + 2],
+            QFF_KL[i : i + 2],
+            lw=5,
+            color="r",
+            alpha=np.clip(shval[i, 5], 0, 1),
+        )
+        ax3.plot(
+            tt[i : i + 2],
+            QFF_KL[i : i + 2],
+            lw=5,
+            color="b",
+            alpha=np.abs(np.clip(shval[i, 5], -1, 0)),
+        )
+
+    ax2.hlines(0, tt.min(), tt.max(), ls=":", color="black")
+    ax2.set_ylim([-15, 15])
+    ax2.set_xlim([tt.min(), tt.max()])
+    ax2.set_ylabel(
+        "KLO-GVE [hPa]",
+        fontsize="small",
+    )
+    ax2.set_xticklabels([])
+    ax3.hlines(0, tt.min(), tt.max(), ls=":", color="black")
+    ax3.set_ylim([-15, 15])
+    ax3.set_xlim([tt.min(), tt.max()])
+    ax3.set_ylabel(
+        "KLO-LUG [hPa]",
+        fontsize="small",
+    )
+    ax3.tick_params(axis="x", labelrotation=45, labelsize="small")
+    ax3.xaxis.set_major_formatter(DateFormatter("%a %H"))
+
+    plt.tight_layout()
+
     return fig
 
 
